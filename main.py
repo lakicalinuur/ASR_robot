27dd083469e4a08cde212832aee0fa23b8020e97
Laki <lakicalinuur@icloud.com>
2025-12-28 13:39:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -347,7 +347,7 @@ def handle_media(message):
     if not media:
         return
     if getattr(media, 'file_size', 0) > MAX_UPLOAD_SIZE:
-        bot.reply_to(message, f"Just send me a file less than {MAX_UPLOAD_MB}MB 😎")
+        bot.reply_to(message, f"Just send me a file less than {MAX_UPLOAD_MB}MB 😎 or use @MediaToTextBot")
         return
     bot.send_chat_action(message.chat.id, 'typing')
     try:
